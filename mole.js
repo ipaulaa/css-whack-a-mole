c41ead4dc74ad2613cd76de936c2ded1e0b2bfc2
Paula Rodrigues <ipaulaa.dev@gmail.com>
2024-11-21 06:24:44 -0300

    test: test animation

--- mole.js
+++ mole.js
@@ -61,3 +61,12 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+window.addEventListener("DOMContentLoaded", () => {
+  setInterval(() => {
+    const moleHeads = document.querySelectorAll(".wgs__mole-head");
+    for (let moleHead of moleHeads) {
+      moleHead.classList.toggle("wgs__mole-head--hidden");
+    }
+  }, 1000);
+});
